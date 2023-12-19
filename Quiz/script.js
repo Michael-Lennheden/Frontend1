4551ef1f275b56b34c31a6046de58cf72737877d
Michael Lennheden <michael@lennheden.com>
2023-12-19 21:17:05 +0100

    Added a progress bar and more questions

--- Quiz/script.js
+++ Quiz/script.js
@@ -3,15 +3,89 @@ const questionContainer = document.querySelector('.question-container')
 const btnStart = document.querySelector('.startBtn')
 const scoreParagraph = document.querySelector('.score')
 const roundParagraph = document.querySelector('.round')
+const progressBarValue = document.querySelector('.progressBar')
 let randomisedQuestions = undefined
 let currentQuestion = undefined
-
 let score = 0
 let round = 1
 scoreParagraph.innerText = 'Score: ' + score
 roundParagraph.innerText = 'Round: ' + round
 
 btnStart.addEventListener('click', startQuiz)
+
+function startQuiz() {
+    console.log('Quiz started')
+    if (questions.length === 0) {
+        console.error('No questions available.')
+        return
+    }
+    randomisedQuestions = questions.sort(() => Math.random() - 0.5)
+    currentQuestion = 0
+    setQuestion()
+    btnStart.removeEventListener('click', startQuiz)
+    btnStart.addEventListener('click', restart)
+    btnStart.innerText = 'Restart'
+}
+
+function restart(){
+    location.reload()
+}
+
+function setQuestion() {
+    if (currentQuestion >= 0 && currentQuestion < randomisedQuestions.length) {
+        DisplayQuestion(randomisedQuestions[currentQuestion])
+        SetAnswers(randomisedQuestions[currentQuestion])
+        progressBar()
+    } else {
+        console.error('Invalid question index.')
+    }
+}
+
+function DisplayQuestion(question) {
+    questionContainer.innerText = question.question
+}
+
+function SetAnswers(question) {
+    answerContainer.innerHTML = ''
+
+    let answers = question.answers
+    for (let j = 0; j < answers.length; j++) {
+        const button = document.createElement('button')
+        button.classList.add('answer')
+        button.innerText = answers[j].text
+        button.setAttribute('data-correct', answers[j].correct)
+        button.addEventListener('click', handleAnswerClick)
+        answerContainer.appendChild(button)
+    }
+}
+
+function handleAnswerClick(event) {
+    const selectedAnswer = event.target
+    const isCorrect = selectedAnswer.dataset.correct === 'true'
+    if (isCorrect) {
+        selectedAnswer.classList.add('correct')
+        scoreParagraph.innerText = 'Score: ' + ++score
+    } else {
+        selectedAnswer.classList.add('incorrect')
+    }
+    setTimeout(() => {
+        currentQuestion++
+        roundParagraph.innerText = 'Round: ' + ++round
+        setQuestion()
+    }, 5000)
+}
+
+function progressBar(){
+    let width = 100;
+    let interval = setInterval(function() {
+        if (width <= 0) {
+            clearInterval(interval);
+        } else {
+            width--;
+            progressBarValue.style.width = width + '%';
+        }
+    }, 50);
+}
 
 const questions = [
     {
@@ -21,10 +95,10 @@ const questions = [
             { text: 'Saturn', correct: false},
             { text: 'Jupiter', correct: false},
             { text: 'Venus', correct: false}
-            ]
+        ]
     },
     {
-      question: 'What is the capital city of Australia?',
+        question: 'What is the capital city of Australia?',
         answers: [
             {text: 'Sydney', correct: false},
             {text: 'Melbourne', correct: false},
@@ -58,60 +132,59 @@ const questions = [
             {text: 'Ag', correct: false},
             {text: 'Fe', correct: false}
         ]
+    },
+    {
+        question: 'What is the largest mammal on Earth?',
+        answers:[
+            { text: 'Elephant', correct: false },
+            { text: 'Blue Whale', correct: true },
+            { text: 'Giraffe', correct: false },
+            { text: 'Hippopotamus', correct: false }
+        ]
+    },
+    {
+        question: 'What is the capital city of France?',
+        answers:[
+            { text: 'Berlin', correct: false },
+            { text: 'Madrid', correct: false },
+            { text: 'Paris', correct: true },
+            { text: 'Rome', correct: false }
+        ]
+    },
+    {
+        question: 'In which year did the United States declare its independence?',
+        answers:[
+            { text: '1776', correct: true },
+            { text: '1789', correct: false },
+            { text: '1800', correct: false },
+            { text: '1820', correct: false }
+        ]
+    },
+    {
+        question: 'What is the largest ocean on Earth?',
+        answers:[
+            { text: 'Atlantic Ocean', correct: false },
+            { text: 'Indian Ocean', correct: false },
+            { text: 'Arctic Ocean', correct: false },
+            { text: 'Pacific Ocean', correct: true }
+        ]
+    },
+    {
+        question: 'Who painted the Mona Lisa?',
+        answers:[
+            { text: 'Vincent van Gogh', correct: false },
+            { text: 'Leonardo da Vinci', correct: true },
+            { text: 'Pablo Picasso', correct: false },
+            { text: 'Michelangelo', correct: false }
+        ]
+    },
+    {
+        question: 'What is the capital city of Japan?',
+        answers:[
+            { text: 'Seoul', correct: false },
+            { text: 'Beijing', correct: false },
+            { text: 'Tokyo', correct: true },
+            { text: 'Bangkok', correct: false }
+        ]
     }
 ]
-
-function startQuiz() {
-    console.log('Quiz started')
-    if (questions.length === 0) {
-        console.error('No questions available.')
-        return
-    }
-
-    randomisedQuestions = questions.sort(() => Math.random() - 0.5)
-    currentQuestion = 0
-    setQuestion()
-}
-
-function setQuestion() {
-    if (currentQuestion >= 0 && currentQuestion < randomisedQuestions.length) {
-        DisplayQuestion(randomisedQuestions[currentQuestion])
-        SetAnswers(randomisedQuestions[currentQuestion])
-    } else {
-        console.error('Invalid question index.')
-    }
-}
-function DisplayQuestion(question) {
-    questionContainer.innerText = question.question
-}
-function SetAnswers(question) {
-    answerContainer.innerHTML = ''
-
-    let answers = question.answers
-    for (let j = 0; j < answers.length; j++) {
-        const button = document.createElement('button')
-        button.classList.add('answer')
-        button.innerText = answers[j].text
-        button.setAttribute('data-correct', answers[j].correct)
-        button.addEventListener('click', handleAnswerClick)
-        answerContainer.appendChild(button)
-    }
-}
-
-function handleAnswerClick(event) {
-    const selectedAnswer = event.target
-    const isCorrect = selectedAnswer.dataset.correct === 'true'
-
-    if (isCorrect) {
-        selectedAnswer.classList.add('correct')
-        scoreParagraph.innerText = 'Score: ' + ++score
-    } else {
-        selectedAnswer.classList.add('incorrect')
-    }
-
-    setTimeout(() => {
-        currentQuestion++
-        roundParagraph.innerText = 'Round: ' + ++round
-        setQuestion()
-    }, 5000)
-}
